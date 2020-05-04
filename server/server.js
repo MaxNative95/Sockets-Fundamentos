c2f798eecbfdff177749cd1398a5a2fb33031801
Max Garcia <maxgarcia@Maxs-MacBook-Pro.local>
2020-05-04 17:46:18 -0600

    primer commit

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
+const path = require('path');
+
+const app = express();
+let server = http.createServer(app)
+
+const publicPath = path.resolve(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+let io = socketIO(server)
+
+server.listen(port, (err) => {
+
+    if (err) throw new Error(err);
+
+    console.log(`Servidor corriendo en puerto ${ port }`);
+
+});
