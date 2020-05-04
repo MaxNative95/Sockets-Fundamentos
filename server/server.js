eee801a93a9fa7ea5bc4905142bd23b9e7473762
Max Garcia <maxgarcia@Maxs-MacBook-Pro.local>
2020-05-04 17:50:49 -0600

    segundo commit

--- server/server.js
+++ server/server.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const socketIO = require('socket.io');
 const http = require('http');
+
 const path = require('path');
 
 const app = express();
-let server = http.createServer(app)
+let server = http.createServer(app);
 
 const publicPath = path.resolve(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-let io = socketIO(server)
+// IO = esta es la comunicacion del backend
+module.exports.io = socketIO(server);
+require('./sockets/socket');
+
+
+
+
 
 server.listen(port, (err) => {
 
